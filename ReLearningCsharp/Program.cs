290a82c8b5564f2fae08f86b496f36ea5ccf6a48
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-04-22 13:34:39 +0200

    Add project files.

---
+++ ReLearningCsharp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReLearningCsharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student ahmet = new Student("Ahmet", 24, 70.50);
+            Console.WriteLine( ahmet.GetName());
+
+
+        }
+
+
+    }
+}
---
+++ ReLearningCsharp/Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReLearningCsharp
+{
+    public class Student
+    {
+        private string Name;
+        private int Age;
+        private double FinalGrade;
+
+        public Student(string name,int age,double finalGrade)
+        {
+            Name = name;
+            Age = age;
+            FinalGrade= finalGrade;
+        }
+
+        public Student(string name,int age) : this(name,age,0)
+        {
+            
+        }
+        public string GetName()
+        {
+            if (Name.StartsWith("A") || Name.StartsWith("a")) return Name;
+            return "no data found";
+            
+        }
+    }
+}
