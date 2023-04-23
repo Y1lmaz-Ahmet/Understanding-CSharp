81596650c1819a0289977633d2a5b7b2a07cbab1
y1lmaz-Ahmet <ay0201998@gmail.com>
2023-04-23 14:06:31 +0200

    making a static method for a birthCalculator of a person

--- ReLearningCsharp/Program.cs
+++ ReLearningCsharp/Program.cs
@@ -24,13 +24,35 @@ namespace ReLearningCsharp
             //Laura.setName("jan");
             //Console.WriteLine(Laura.GetName());
 
-            BankAccount bank1 = new BankAccount(50, "ahmet");
-            BankAccount bank2 = new BankAccount(150, "atilla");
+            //BankAccount bank1 = new BankAccount(50, "ahmet");
+            //BankAccount bank2 = new BankAccount(150, "atilla");
 
-            BankAccount bank3 = bank1 + bank2;
-            Console.WriteLine(bank3.MoneyOwner);
-            Console.WriteLine(bank3.Money);
+            //BankAccount bank3 = bank1 + bank2;
+            //Console.WriteLine(bank3.MoneyOwner);
+            //Console.WriteLine(bank3.Money);
 
+            //timespan calculator
+            birthCalculator(1998,12,7);
+
+        }
+        public static void birthCalculator(int birthYear,int monthOfBirth,int dayOfBirth)
+        {
+            Console.WriteLine("enter year of birth:");
+             birthYear = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter month of birth:");
+             monthOfBirth = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter day of birth:");
+             dayOfBirth = int.Parse(Console.ReadLine());
+
+            DateTime personBirthDate = new DateTime(birthYear, monthOfBirth, dayOfBirth);
+
+            TimeSpan timespan = DateTime.Now - personBirthDate;
+
+            Console.WriteLine($"the birthDate:{personBirthDate}");
+            Console.WriteLine($"days that this person is alive:{timespan}");
+            
         }
 
 
